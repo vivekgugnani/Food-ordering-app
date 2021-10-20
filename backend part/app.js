31c67e7c2b8573ad08abad2ec85eac60e137600d
Vivek <vivek0094.cse19@chitkara.edu.in>
2021-10-20 10:33:25 +0530

    added backend

---
+++ backend part/app.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const seedDB = require('./seed');
+const cors = require('cors');
+
+mongoose.connect(process.env.MONGODB)
+    .then(() => {
+        console.log('DB Connected');
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+
+//seedDB();
+app.use(express.json());
+app.use(cors(
+    {
+        origin: ['http://localhost:3000','https://petuu.netlify.app'],
+        credentials:true
+    },
+))
+
+
+const foodRoutes = require('./api/foodRoutes');
+
+
+app.get('/hello', (req, res) => {
+    
+    res.status(200).json({msg:'Hello from the server'})
+})
+
+
+
+
+
+
+app.use(foodRoutes);
+
+
+const port = process.env.PORT || 8000
+
+app.listen(port, () => {
+    console.log(`server running at ${port}`)
+})
